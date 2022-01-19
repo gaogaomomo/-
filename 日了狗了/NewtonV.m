80f343f41bdb7f59b7fd7e6a59f2fcddae6eb4d9
Mohan Gao <92876313+gaogaomomo@users.noreply.github.com>
2022-01-19 22:33:18 +0100

    19.01.2022

--- 日了狗了/NewtonV.m
+++ 日了狗了/NewtonV.m
@@ -1,24 +1,30 @@
 function[z,iter,b] = NewtonV(x0,tol,maxiter)
 h=10^(-6);
 n=length(x0);
-J=zeros(n);
 e=eye(n);
-for j=1:n
-    x=x0-f(x0)/(round(f(x0+h*e(:,j))-f(x0-h*e(:,j))/(2*h)));
-    J(:,j)=round(f(x+h*e(:,j))-f(x-h*e(:,j))/(2*h));    
-    while norm(x(j+1)-x(j))>tol && j<=maxiter+1
-          x=x-f(x)/J(:,j);
-    end
-    iter=j;
-    z=x;
-    if iter == maxiter+1
-        b=0;
-    else 
-        b=1;
-    end
-end
+x=x0;
+J=zeros(n,n);
 
+iter=1;%iter fängt mit 1 an
+while true %Die Iteration soll immer weitergehen, bevor die abgebrochen wird.
+    xAlt=x;
+    for i=1:n
+        J(:,i)=(f(x+h*e(:,i))-f(x-h*e(:,i)))/(2*h);%Approximation der Jacobi-matrix
+    end
+    x=x-J\f(x);
+    iter=iter+1;
+    if norm(x-xAlt)<=tol || iter>=maxiter+1
+        if iter == maxiter
+            b=0;
+        else
+            b=1;
+        end
+        z=x;
+        break
+    end
+
+end
 function[y] = f(x)
-y(1)=x(1)^2+x(2)^2+(3/5)*x(2)-(4/25);
-y(2)=x(1)^2-x(2)^2+x(1)-(8/5)*x(2)-(7/50);
+y(1)=x(1)^2+x(2)^2+(3/5)*x(2)-4/25;
+y(2)=x(1)^2-x(2)^2+x(1)-(8/5)*x(2)-7/50;
 y = [y(1);y(2)];
--- 日了狗了/mainNV.m
+++ 日了狗了/mainNV.m
@@ -1,4 +1,6 @@
-x0=[1 1];
 maxiter=100;
 tol=10^(-5);
-[z,iter,b] = NewtonV(x0,tol,maxiter);
+x1=[1,1]';
+x2=[-1,-1]';
+[z1,iter1,b1] = NewtonV(x1,tol,maxiter);
+[z2,iter2,b2] = NewtonV(x2,tol,maxiter);
